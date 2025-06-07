a28a435a71d06aae96090013a95201f21d657942
zannimo <luigi.rizzo@hotmail.it>
2025-06-07 20:12:10 +0200

    forgot to add this in the previous commit

--- data_infra/upload_frontend.py
+++ data_infra/upload_frontend.py
@@ -1,5 +1,4 @@
 import boto3
-import os
 import subprocess
 
 AWS_REGION = "us-east-1"
@@ -12,8 +11,12 @@ def get_output(name):
 STATIC_S3_BUCKET = get_output("static_s3_bucket_name")
 
 # AWS Clients
-s3_client = boto3.client("s3") 
+s3 = boto3.client("s3") 
 
-# ... 
+# Upload index.html and error.html
+s3.upload_file("../frontend/templates/index.html", STATIC_S3_BUCKET, "index.html", ExtraArgs={"ContentType": "text/html"})
+s3.upload_file("../frontend/templates/error.html", STATIC_S3_BUCKET, "error.html", ExtraArgs={"ContentType": "text/html"})
 
-
+# Upload JS and CSS under static/
+s3.upload_file("../frontend/static/app.js", STATIC_S3_BUCKET, "static/app.js", ExtraArgs={"ContentType": "application/javascript"})
+s3.upload_file("../frontend/static/style.css", STATIC_S3_BUCKET, "static/style.css", ExtraArgs={"ContentType": "text/css"})
